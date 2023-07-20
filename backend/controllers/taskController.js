314eaff7f7bcb6d67feb61663263cdbe07845c43
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 10:59:21 +0300

    Get a single task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -17,9 +17,23 @@ const getTasks = async(req,res) => {
     res.status(500).json({msg: error.message})
   }
 }
+const getTask = async(req,res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if(!task) {
+      return res.status(404).json(`No task with id ${id}`)
+    }
+    res.status(200).json(task)
+    
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
+}
 
 
 module.exports = {
   createTask,
-  getTasks
+  getTasks,
+  getTask
 }
