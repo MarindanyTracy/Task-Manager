14962d4d4351c05eb4b62ce959bc97d38ca0a431
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 15:37:22 +0300

    Create all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import TaskList from "./components/TaskList";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="task-container">
+        <TaskList />
+      </div>
+      <ToastContainer />
     </div>
   );
 }
