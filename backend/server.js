13b082e3c711cb14e8067258e1fa425e280e4420
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 16:44:48 +0300

    Adding task to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 const mongoose = require("mongoose");
 const Task = require('./models/taskModel');
 const taskRoutes = require('./routes/taskRoute');
@@ -9,6 +10,8 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors())
+
 app.use('/api/tasks',taskRoutes);
 
 //Routes
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import TaskForm from './TaskForm'
+import React, { useState } from 'react'
+import axios from 'axios';
+import TaskForm from './TaskForm';
+import {toast} from 'react-toastify';
 import Task from './Task'
 
 const TaskList = () => {
+  const [formData,setFormData] = useState({
+    name: '',
+    completed: false
+  })
+
+  const { name } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value})
+  }
+
+  const createTask = async(e) => {
+    e.preventDefault();
+    if(name === '') {
+      return toast.error('Input field can\'t be empty');
+    }
+    try {
+      await axios.post('http://localhost:5000/api/tasks', formData);
+      toast.success('Task added successfully')
+      setFormData({...formData, name: ""})
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
   return (
     <div>
       <h2>Task Manager</h2>
-      <TaskForm />
+      <TaskForm name={ name } handleInputChange={handleInputChange} createTask={createTask} />
       <div className='--flex-between --pb'>
         <p><b>Total tasks: </b> 0</p>
         <p><b>Completed tasks: </b> 0</p>
