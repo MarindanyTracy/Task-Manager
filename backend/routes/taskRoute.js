0497dc99d8c88e9f40dda0799b4f4ff24d9bd9ae
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 10:46:49 +0300

    Separating concerns to task controller

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const Task = require("../models/taskModel");
+const { createTask, getTasks } = require("../controllers/taskController");
+
+
 const router = express.Router();
 
-//Create a task
-router.post('/api/tasks', async(req,res) => {
-  try {
-   const task = await Task.create(req.body)
-   res.status(201).json(task)
-  } catch (error) {
-   res.status(500).json({msg: error.message})
-  }
- })
+router.post('/api/tasks', createTask)
 
- //Read data
-router.get('/api/tasks', async (req,res) => {
-  try {
-    const tasks = await Task.find()
-    res.status(200).json(tasks)
-  } catch (error) {
-    res.status(500).json({msg: error.message})
-  }
-})
+router.get('/api/tasks', getTasks)
 
 
 module.exports = router;
