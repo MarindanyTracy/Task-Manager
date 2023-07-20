2d7e06a97d355cbbd164c750c234474072e984e6
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 10:05:49 +0300

    Creating a task route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
 
 const app = express();
 
+//Routes
 app.get("/", (req, res) => {
   res.send("Home page");
 });
+//Create a task
+app.post('/api/tasks', async(req,res) => {
+  console.log(req.body)
+  res.send('Task Created')
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
