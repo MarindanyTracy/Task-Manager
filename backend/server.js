61d5da53c184e799d7dbe4a46a1466dede2cefb1
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 10:22:32 +0300

    Middlewares and saving tasks to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const Task = require('./models/taskModel');
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 //Routes
 app.get("/", (req, res) => {
@@ -10,9 +15,14 @@ app.get("/", (req, res) => {
 });
 //Create a task
 app.post('/api/tasks', async(req,res) => {
-  console.log(req.body)
-  res.send('Task Created')
+ try {
+  const task = await Task.create(req.body)
+  res.status(201).json(task)
+ } catch (error) {
+  res.status(500).json({msg: error.message})
+ }
 })
+
 
 const PORT = process.env.PORT || 5000;
 
