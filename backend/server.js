8c6728c7433c4339a7e2c03e2ac2f7f89b7fa002
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 10:39:22 +0300

    Creating Task Route

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const Task = require('./models/taskModel');
+const taskRoutes = require('./routes/taskRoute');
 
 const app = express();
 
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
+app.use(taskRoutes);
 
 //Routes
 app.get("/", (req, res) => {
   res.send("Home page");
 });
-//Create a task
-app.post('/api/tasks', async(req,res) => {
- try {
-  const task = await Task.create(req.body)
-  res.status(201).json(task)
- } catch (error) {
-  res.status(500).json({msg: error.message})
- }
-})
-//Read data
-app.get('/api/tasks', async (req,res) => {
-  try {
-    const tasks = await Task.find()
-    res.status(200).json(tasks)
-  } catch (error) {
-    res.status(500).json({msg: error.message})
-  }
-})
 
 const PORT = process.env.PORT || 5000;
 
