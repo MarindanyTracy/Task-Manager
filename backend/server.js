315f8bbd8b99ad2246127ce5a922b76cbe679451
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 09:36:20 +0300

    Connecting to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const dotenv = require("dotenv").config()
+const connectDB = require('./config/connectDB')
 
 const app = express();
 
+app.get('/', (req,res) => {
+  res.send('Home page')
+})
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
