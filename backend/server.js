f578a580cec8be7243ac1aacf7ffa225a344dcb6
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-20 09:39:27 +0300

    Refactoring the connection sequence

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,17 @@ const app = express();
 app.get('/', (req,res) => {
   res.send('Home page')
 })
-connectDB();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
